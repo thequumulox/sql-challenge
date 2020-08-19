18b7e27f64382df78c50d1a22a8dfd616f417e26
thequumulox <59245984+thequumulox@users.noreply.github.com>
2020-08-19 00:03:32 -0700

    Update create_table.sql

--- create_table.sql
+++ create_table.sql
@@ -1,1 +1,74 @@
 
+Database: Employees
+
+-- DROP DATABASE "Employees";
+
+CREATE DATABASE "Employee"
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+
+-- Table inventory from employee CSV data
+
+CREATE TABLE "Employee" (
+    "emp_no" int   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(255)   NOT NULL,
+    "last_name" varchar(255)   NOT NULL,
+    "gender" varchar(255)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_Employee" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+-- Bridge Table
+CREATE TABLE "EmployeeDepartment" (
+    "emp_no" int   NOT NULL,
+    "dept_no" int   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "DepartmentManager" (
+    "dept_no" int   NOT NULL,
+    "emp_no" int   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "Department" (
+    "dept_no" int   NOT NULL,
+    "dept_name" varchar(200)   NOT NULL,
+    CONSTRAINT "pk_Department" PRIMARY KEY (
+        "dept_no"
+     ),
+    CONSTRAINT "uc_Department_dept_name" UNIQUE (
+        "dept_name"
+    )
+);
+
+CREATE TABLE "Salary" (
+    "emp_no" int   NOT NULL,
+    "salary" money   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+CREATE TABLE "Title" (
+    "emp_no" int   NOT NULL,
+    "title" varchar(255)   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL
+);
+
+ALTER TABLE "Employee" ADD CONSTRAINT "fk_Employee_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "EmployeeDepartment" ("emp_no");
+
+ALTER TABLE "Department" ADD CONSTRAINT "fk_Department_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "DepartmentManager" ("dept_no");
